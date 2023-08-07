310f388db1302ca149f28760f0b65692ad278741
AdhithyaGN <adhithyagnair97@gmail.com>
2023-08-07 12:01:01 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+
+hyphendot="-e ."
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    
+    '''
+ 
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+
+        if hyphendot in requirements:
+            requirements.remove(hyphendot)
+
+    return requirements
+
+
+
+
+setup(
+name="MLProject1",
+version="0.0.1",
+author="Adhithya G Nair",
+author_email="adhithyagnair97@gmail.com",
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+
+
+
+
+)
